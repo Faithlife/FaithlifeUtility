62e28fc3cbc5d104a29a962b35090f7337a50c6b
Ed Ball <ed.ball@faithlife.com>
2017-09-26 09:22:09 -0700

    KeyEqualityComparer: Code style.

--- src/Faithlife.Utility/KeyEqualityComparer.cs
+++ src/Faithlife.Utility/KeyEqualityComparer.cs
@@ -14,25 +14,11 @@ namespace Faithlife.Utility
 		/// Initializes a new instance of the KeyEqualityComparer class.
 		/// </summary>
 		/// <param name="keySelector">The key selector delegate</param>
-		public KeyEqualityComparer(Func<TSource, TKey> keySelector) :
-			this(keySelector, EqualityComparer<TKey>.Default)
-		{
-		}
-
-		/// <summary>
-		/// Initializes a new instance of the KeyEqualityComparer class.
-		/// </summary>
-		/// <param name="keySelector">The key selector delegate</param>
 		/// <param name="keyComparer">The IEqualityComparer used for comparison of keys</param>
-		public KeyEqualityComparer(Func<TSource, TKey> keySelector, IEqualityComparer<TKey> keyComparer)
+		public KeyEqualityComparer(Func<TSource, TKey> keySelector, IEqualityComparer<TKey> keyComparer = null)
 		{
-			if (keySelector == null)
-				throw new ArgumentNullException(nameof(keySelector));
-			if (keyComparer == null)
-				throw new ArgumentNullException(nameof(keyComparer));
-
-			m_keySelector = keySelector;
-			m_keyComparer = keyComparer;
+			m_keySelector = keySelector ?? throw new ArgumentNullException(nameof(keySelector));
+			m_keyComparer = keyComparer ?? EqualityComparer<TKey>.Default;
 		}
 
 		/// <summary>
@@ -43,12 +29,7 @@ namespace Faithlife.Utility
 		/// <returns>
 		/// true if the specified objects are equal; otherwise, false.
 		/// </returns>
-		public override bool Equals(TSource x, TSource y)
-		{
-			TKey k1 = m_keySelector(x);
-			TKey k2 = m_keySelector(y);
-			return m_keyComparer.Equals(k1, k2);
-		}
+		public override bool Equals(TSource x, TSource y) => m_keyComparer.Equals(m_keySelector(x), m_keySelector(y));
 
 		/// <summary>
 		/// Serves as a hash function for the specified object for hashing algorithms and data structures, such as a hash table.
@@ -56,10 +37,7 @@ namespace Faithlife.Utility
 		/// <param name="obj">The object for which to get a hash code.</param>
 		/// <returns>A hash code for the specified object.</returns>
 		/// <exception cref="T:System.ArgumentNullException">The type of <paramref name="obj"/> is a reference type and <paramref name="obj"/> is null.</exception>
-		public override int GetHashCode(TSource obj)
-		{
-			return obj == null ? 0 : m_keyComparer.GetHashCode(m_keySelector(obj));
-		}
+		public override int GetHashCode(TSource obj) => obj == null ? 0 : m_keyComparer.GetHashCode(m_keySelector(obj));
 
 		readonly Func<TSource, TKey> m_keySelector;
 		readonly IEqualityComparer<TKey> m_keyComparer;
