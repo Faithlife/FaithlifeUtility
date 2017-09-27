3c5b7e32e55ebe523d32088a622237a41aa009d1
Ed Ball <ed.ball@faithlife.com>
2017-09-27 09:20:14 -0700

    ObjectUtility: Code style.

--- src/Faithlife.Utility/ObjectUtility.cs
+++ src/Faithlife.Utility/ObjectUtility.cs
@@ -13,10 +13,7 @@ namespace Faithlife.Utility
 		/// </summary>
 		/// <param name="obj">The object for which to get a hash code.</param>
 		/// <returns>The hash code for the specified object, or zero if the object is null.</returns>
-		public static int GetHashCode<T>(T obj)
-		{
-			return obj == null ? 0 : obj.GetHashCode();
-		}
+		public static int GetHashCode<T>(T obj) => obj == null ? 0 : obj.GetHashCode();
 
 		/// <summary>
 		/// Creates an equality comparer from delegates.
@@ -25,10 +22,7 @@ namespace Faithlife.Utility
 		/// <param name="equals">The equals delegate.</param>
 		/// <returns>The equality comparer.</returns>
 		/// <remarks>If GetHashCode is called, it will throw a NotImplementedException.</remarks>
-		public static EqualityComparer<T> CreateEqualityComparer<T>(Func<T, T, bool> equals)
-		{
-			return new GenericEqualityComparer<T>(equals);
-		}
+		public static EqualityComparer<T> CreateEqualityComparer<T>(Func<T, T, bool> equals) => new GenericEqualityComparer<T>(equals);
 
 		/// <summary>
 		/// Creates an equality comparer from delegates.
@@ -38,9 +32,6 @@ namespace Faithlife.Utility
 		/// <param name="getHashCode">The hash code delegate.</param>
 		/// <returns>The equality comparer.</returns>
 		/// <remarks>If getHashCode is null and GetHashCode is called, it will throw a NotImplementedException.</remarks>
-		public static EqualityComparer<T> CreateEqualityComparer<T>(Func<T, T, bool> equals, Func<T, int> getHashCode)
-		{
-			return new GenericEqualityComparer<T>(equals, getHashCode);
-		}
+		public static EqualityComparer<T> CreateEqualityComparer<T>(Func<T, T, bool> equals, Func<T, int> getHashCode) => new GenericEqualityComparer<T>(equals, getHashCode);
 	}
 }
