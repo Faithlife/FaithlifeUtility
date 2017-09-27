0bb07d10d43e9b3c48c2d10f5e04a5970c2acd57
Ed Ball <ed.ball@faithlife.com>
2017-09-27 09:25:16 -0700

    ReadOnlyStream: Code style.

--- src/Faithlife.Utility/ReadOnlyStream.cs
+++ src/Faithlife.Utility/ReadOnlyStream.cs
@@ -21,52 +21,34 @@ namespace Faithlife.Utility
 		/// Gets a value indicating whether the current stream supports writing.
 		/// </summary>
 		/// <returns><c>true</c> if the stream supports writing; otherwise, <c>false</c>.</returns>
-		public override bool CanWrite
-		{
-			get { return false; }
-		}
+		public override bool CanWrite => false;
 
 		/// <summary>
 		/// Sets the length of the current stream.
 		/// </summary>
-		public override void SetLength(long value)
-		{
-			throw new NotSupportedException();
-		}
+		public override void SetLength(long value) => throw new NotSupportedException();
 
 		/// <summary>
 		/// Writes a sequence of bytes to the current stream and advances the current position
 		/// within this stream by the number of bytes written.
 		/// </summary>
-		public override void Write(byte[] buffer, int offset, int count)
-		{
-			throw new NotSupportedException();
-		}
+		public override void Write(byte[] buffer, int offset, int count) => throw new NotSupportedException();
 
 		/// <summary>
 		/// Writes a byte to the current position in the stream and advances the position within the stream by one byte.
 		/// </summary>
-		public override void WriteByte(byte value)
-		{
-			throw new NotSupportedException();
-		}
+		public override void WriteByte(byte value) => throw new NotSupportedException();
 
 #if !NETSTANDARD1_4
 		/// <summary>
 		/// Begins an asynchronous write operation.
 		/// </summary>
-		public override IAsyncResult BeginWrite(byte[] buffer, int offset, int count, AsyncCallback callback, object state)
-		{
-			throw new NotSupportedException();
-		}
+		public override IAsyncResult BeginWrite(byte[] buffer, int offset, int count, AsyncCallback callback, object state) => throw new NotSupportedException();
 
 		/// <summary>
 		/// Ends an asynchronous write operation.
 		/// </summary>
-		public override void EndWrite(IAsyncResult asyncResult)
-		{
-			throw new NotSupportedException();
-		}
+		public override void EndWrite(IAsyncResult asyncResult) => throw new NotSupportedException();
 #endif
 	}
 }
