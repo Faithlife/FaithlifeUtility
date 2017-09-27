b9acdcab2e152fc358260d99a39a504ecbbd0a21
Ed Ball <ed.ball@faithlife.com>
2017-09-27 09:43:32 -0700

    StreamUtility: Remove methods.
    * Remove CopyStream (use Stream.CopyTo).
    * Remove WriteBatched (obscure).
    * Remove CopyToMemoryStream (obscure).

--- src/Faithlife.Utility/StreamUtility.cs
+++ src/Faithlife.Utility/StreamUtility.cs
@@ -1,6 +1,5 @@
 using System;
 using System.IO;
-using Faithlife.Utility.Threading;
 
 namespace Faithlife.Utility
 {
@@ -10,153 +9,6 @@ namespace Faithlife.Utility
 	public static class StreamUtility
 	{
 		/// <summary>
-		/// Copies all the data after the current position in the source stream to the destination stream.
-		/// </summary>
-		/// <param name="fromStream">The source stream.</param>
-		/// <param name="toStream">The destination stream.</param>
-		/// <returns>The number of bytes actually copied.</returns>
-		public static long CopyStream(Stream fromStream, Stream toStream)
-		{
-			return CopyStream(fromStream, toStream, long.MaxValue, c_nStandardBufferSize);
-		}
-
-		/// <summary>
-		/// Copies up to the specified number of bytes from the source stream to the destination stream.
-		/// </summary>
-		/// <param name="fromStream">The source stream.</param>
-		/// <param name="toStream">The destination stream.</param>
-		/// <param name="bytesToCopy">The maximum number of bytes to copy.</param>
-		/// <returns>The number of bytes actually copied.</returns>
-		public static long CopyStream(Stream fromStream, Stream toStream, long bytesToCopy)
-		{
-			return CopyStream(fromStream, toStream, bytesToCopy, c_nStandardBufferSize);
-		}
-
-		/// <summary>
-		/// Copies up to the specified number of bytes from the source stream to the destination stream.
-		/// </summary>
-		/// <param name="fromStream">The source stream.</param>
-		/// <param name="toStream">The destination stream.</param>
-		/// <param name="bytesToCopy">The maximum number of bytes to copy.</param>
-		/// <param name="bufferSize">The internal buffer size to use when copying.</param>
-		/// <returns>The number of bytes actually copied.</returns>
-		public static long CopyStream(Stream fromStream, Stream toStream, long bytesToCopy, int bufferSize)
-		{
-			return CopyStream(fromStream, toStream, bytesToCopy, bufferSize, WorkState.None);
-		}
-
-		/// <summary>
-		/// Copies up to the specified number of bytes from the source stream to the destination stream.
-		/// </summary>
-		/// <param name="fromStream">The source stream.</param>
-		/// <param name="toStream">The destination stream.</param>
-		/// <param name="bytesToCopy">The maximum number of bytes to copy.</param>
-		/// <param name="bufferSize">The internal buffer size to use when copying.</param>
-		/// <param name="workState">The work state.</param>
-		/// <returns>The number of bytes actually copied.</returns>
-		public static long CopyStream(Stream fromStream, Stream toStream, long bytesToCopy, int bufferSize, IWorkState workState)
-		{
-			return CopyStream(fromStream, toStream, bytesToCopy, bufferSize, workState, null);
-		}
-
-		/// <summary>
-		/// Copies up to the specified number of bytes from the source stream to the destination stream.
-		/// </summary>
-		/// <param name="fromStream">The source stream.</param>
-		/// <param name="toStream">The destination stream.</param>
-		/// <param name="bytesToCopy">The maximum number of bytes to copy.</param>
-		/// <param name="bufferSize">The internal buffer size to use when copying.</param>
-		/// <param name="workState">The work state.</param>
-		/// <param name="notifier">
-		///	 Pass in an Action to be called as the stream is copied (gives a chance to update progress). 
-		///	 The long passed to the action represents the current total of bytes copied.</param>
-		/// <returns>The number of bytes actually copied.</returns>
-		public static long CopyStream(Stream fromStream, Stream toStream, long bytesToCopy, int bufferSize, IWorkState workState, Action<long> notifier)
-		{
-			if (fromStream == null)
-				throw new ArgumentNullException(nameof(fromStream));
-			if (toStream == null)
-				throw new ArgumentNullException(nameof(toStream));
-
-			long nTotalBytesCopied = 0;
-
-			// do nothing if no bytes to copy
-			if (bytesToCopy > 0)
-			{
-				// allocate temporary buffer
-				if (bufferSize > bytesToCopy)
-					bufferSize = (int) bytesToCopy;
-				byte[] abyBuffer = new byte[bufferSize];
-
-				// transfer data from input stream to output stream
-				while (bytesToCopy > 0 && !workState.Canceled)
-				{
-					// determine bytes to read
-					int nBytesToRead = bytesToCopy < bufferSize ? (int) bytesToCopy : bufferSize;
-
-					// read from input stream
-					int nBytesRead = fromStream.Read(abyBuffer, 0, nBytesToRead);
-
-					// stop loop if done
-					if (nBytesRead == 0)
-						break;
-
-					// write bytes to output stream
-					toStream.Write(abyBuffer, 0, nBytesRead);
-
-					// update number of bytes to copy
-					nTotalBytesCopied += nBytesRead;
-					bytesToCopy -= nBytesRead;
-
-					if (notifier != null)
-						notifier(nTotalBytesCopied);
-				}
-			}
-
-			return nTotalBytesCopied;
-		}
-
-		/// <summary>
-		/// Writes the specified bytes to the stream, one batch at a time.
-		/// </summary>
-		/// <param name="stream">The stream.</param>
-		/// <param name="bytes">The bytes.</param>
-		/// <param name="byteIndex">The byte index.</param>
-		/// <param name="byteCount">The byte count.</param>
-		/// <param name="batchByteCount">The number of bytes per batch.</param>
-		/// <param name="workState">The work state.</param>
-		public static void WriteBatched(this Stream stream, byte[] bytes, int byteIndex, int byteCount, int batchByteCount, IWorkState workState)
-		{
-			if (stream == null)
-				throw new ArgumentNullException(nameof(stream));
-
-			while (byteCount > 0 && !workState.Canceled)
-			{
-				int byteCountToWrite = Math.Min(batchByteCount, byteCount);
-				stream.Write(bytes, byteIndex, byteCountToWrite);
-				byteIndex += byteCountToWrite;
-				byteCount -= byteCountToWrite;
-			}
-		}
-
-		/// <summary>
-		/// Copies the contents of <paramref name="stream"/> to a new <see cref="MemoryStream"/>.
-		/// </summary>
-		/// <param name="stream">The stream to copy.</param>
-		/// <returns>A <see cref="MemoryStream"/> containing all the data read from the stream. The caller is responsible
-		/// for disposing this stream.</returns>
-		/// <remarks><para>The returned <see cref="MemoryStream"/> will be positioned at the first byte.</para>
-		/// <para>This method is useful when <paramref name="stream"/> does not support seeking, but a seekable
-		/// stream is required.</para></remarks>
-		public static MemoryStream CopyToMemoryStream(Stream stream)
-		{
-			MemoryStream memoryStream = new MemoryStream();
-			CopyStream(stream, memoryStream);
-			memoryStream.Position = 0;
-			return memoryStream;
-		}
-
-		/// <summary>
 		/// Reads all bytes from the stream.
 		/// </summary>
 		/// <param name="stream">The stream.</param>
@@ -165,7 +17,7 @@ namespace Faithlife.Utility
 		{
 			using (MemoryStream streamMemory = new MemoryStream())
 			{
-				CopyStream(stream, streamMemory);
+				stream.CopyTo(streamMemory);
 				return streamMemory.ToArray();
 			}
 		}
@@ -265,7 +117,5 @@ namespace Faithlife.Utility
 
 			return stream;
 		}
-
-		private const int c_nStandardBufferSize = 64 * 1024;
 	}
 }
