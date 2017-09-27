96ab39f4d49ceb1124bfecffe600f6623b73d03b
Ed Ball <ed.ball@faithlife.com>
2017-09-27 09:44:50 -0700

    StringBuilderUtility: Code style.

--- src/Faithlife.Utility/StringBuilderUtility.cs
+++ src/Faithlife.Utility/StringBuilderUtility.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Globalization;
 using System.Text;
 
@@ -17,9 +16,7 @@ namespace Faithlife.Utility
 		/// <param name="args">An array of objects to format</param>
 		/// <returns>The <see cref="StringBuilder"/> instance that was provided.</returns>
 		public static StringBuilder AppendFormatInvariant(this StringBuilder stringBuilder, string format, params object[] args)
-		{
-			return stringBuilder.AppendFormat(CultureInfo.InvariantCulture, format, args);
-		}
+			=> stringBuilder.AppendFormat(CultureInfo.InvariantCulture, format, args);
 
 		/// <summary>
 		/// Appends the invariant representation of a specified boolean value to the end of <paramref name="stringBuilder"/>.
@@ -27,9 +24,7 @@ namespace Faithlife.Utility
 		/// <param name="stringBuilder">The <see cref="StringBuilder"/> instance to which the string representation will be appended.</param>
 		/// <param name="value">The value to append.</param>
 		public static StringBuilder AppendInvariant(this StringBuilder stringBuilder, bool value)
-		{
-			return stringBuilder.Append(value.ToString());
-		}
+			=> stringBuilder.Append(value.ToString());
 
 		/// <summary>
 		/// Appends the invariant representation of a specified 8-bit unsigned integer to the end of <paramref name="stringBuilder"/>.
@@ -37,9 +32,7 @@ namespace Faithlife.Utility
 		/// <param name="stringBuilder">The <see cref="StringBuilder"/> instance to which the string representation will be appended.</param>
 		/// <param name="value">The value to append.</param>
 		public static StringBuilder AppendInvariant(this StringBuilder stringBuilder, byte value)
-		{
-			return stringBuilder.Append(value.ToString(CultureInfo.InvariantCulture));
-		}
+			=> stringBuilder.Append(value.ToString(CultureInfo.InvariantCulture));
 
 		/// <summary>
 		/// Appends the invariant representation of a specified decimal number to the end of <paramref name="stringBuilder"/>.
@@ -47,9 +40,7 @@ namespace Faithlife.Utility
 		/// <param name="stringBuilder">The <see cref="StringBuilder"/> instance to which the string representation will be appended.</param>
 		/// <param name="value">The value to append.</param>
 		public static StringBuilder AppendInvariant(this StringBuilder stringBuilder, decimal value)
-		{
-			return stringBuilder.Append(value.ToString(CultureInfo.InvariantCulture));
-		}
+			=> stringBuilder.Append(value.ToString(CultureInfo.InvariantCulture));
 
 		/// <summary>
 		/// Appends the invariant representation of a specified double-precision floating-point number to the end of <paramref name="stringBuilder"/>.
@@ -57,9 +48,7 @@ namespace Faithlife.Utility
 		/// <param name="stringBuilder">The <see cref="StringBuilder"/> instance to which the string representation will be appended.</param>
 		/// <param name="value">The value to append.</param>
 		public static StringBuilder AppendInvariant(this StringBuilder stringBuilder, double value)
-		{
-			return stringBuilder.Append(value.ToString(CultureInfo.InvariantCulture));
-		}
+			=> stringBuilder.Append(value.ToString(CultureInfo.InvariantCulture));
 
 		/// <summary>
 		/// Appends the invariant representation of a specified 16-bit signed integer to the end of <paramref name="stringBuilder"/>.
@@ -67,9 +56,7 @@ namespace Faithlife.Utility
 		/// <param name="stringBuilder">The <see cref="StringBuilder"/> instance to which the string representation will be appended.</param>
 		/// <param name="value">The value to append.</param>
 		public static StringBuilder AppendInvariant(this StringBuilder stringBuilder, short value)
-		{
-			return stringBuilder.Append(value.ToString(CultureInfo.InvariantCulture));
-		}
+			=> stringBuilder.Append(value.ToString(CultureInfo.InvariantCulture));
 
 		/// <summary>
 		/// Appends the invariant representation of a specified 32-bit signed integer to the end of <paramref name="stringBuilder"/>.
@@ -77,9 +64,7 @@ namespace Faithlife.Utility
 		/// <param name="stringBuilder">The <see cref="StringBuilder"/> instance to which the string representation will be appended.</param>
 		/// <param name="value">The value to append.</param>
 		public static StringBuilder AppendInvariant(this StringBuilder stringBuilder, int value)
-		{
-			return stringBuilder.Append(value.ToString(CultureInfo.InvariantCulture));
-		}
+			=> stringBuilder.Append(value.ToString(CultureInfo.InvariantCulture));
 
 		/// <summary>
 		/// Appends the invariant representation of a specified 64-bit signed integer to the end of <paramref name="stringBuilder"/>.
@@ -87,9 +72,7 @@ namespace Faithlife.Utility
 		/// <param name="stringBuilder">The <see cref="StringBuilder"/> instance to which the string representation will be appended.</param>
 		/// <param name="value">The value to append.</param>
 		public static StringBuilder AppendInvariant(this StringBuilder stringBuilder, long value)
-		{
-			return stringBuilder.Append(value.ToString(CultureInfo.InvariantCulture));
-		}
+			=> stringBuilder.Append(value.ToString(CultureInfo.InvariantCulture));
 
 		/// <summary>
 		/// Appends the invariant representation of a specified single-precision floating-point number to the end of <paramref name="stringBuilder"/>.
@@ -97,9 +80,7 @@ namespace Faithlife.Utility
 		/// <param name="stringBuilder">The <see cref="StringBuilder"/> instance to which the string representation will be appended.</param>
 		/// <param name="value">The value to append.</param>
 		public static StringBuilder AppendInvariant(this StringBuilder stringBuilder, float value)
-		{
-			return stringBuilder.Append(value.ToString(CultureInfo.InvariantCulture));
-		}
+			=> stringBuilder.Append(value.ToString(CultureInfo.InvariantCulture));
 
 		/// <summary>
 		/// Appends the invariant representation of a specified 8-bit signed integer to the end of <paramref name="stringBuilder"/>.
@@ -107,9 +88,7 @@ namespace Faithlife.Utility
 		/// <param name="stringBuilder">The <see cref="StringBuilder"/> instance to which the string representation will be appended.</param>
 		/// <param name="value">The value to append.</param>
 		public static StringBuilder AppendInvariant(this StringBuilder stringBuilder, sbyte value)
-		{
-			return stringBuilder.Append(value.ToString(CultureInfo.InvariantCulture));
-		}
+			=> stringBuilder.Append(value.ToString(CultureInfo.InvariantCulture));
 
 		/// <summary>
 		/// Appends the invariant representation of a specified 16-bit unsigned integer to the end of <paramref name="stringBuilder"/>.
@@ -117,9 +96,7 @@ namespace Faithlife.Utility
 		/// <param name="stringBuilder">The <see cref="StringBuilder"/> instance to which the string representation will be appended.</param>
 		/// <param name="value">The value to append.</param>
 		public static StringBuilder AppendInvariant(this StringBuilder stringBuilder, ushort value)
-		{
-			return stringBuilder.Append(value.ToString(CultureInfo.InvariantCulture));
-		}
+			=> stringBuilder.Append(value.ToString(CultureInfo.InvariantCulture));
 
 		/// <summary>
 		/// Appends the invariant representation of a specified 32-bit unsigned integer to the end of <paramref name="stringBuilder"/>.
@@ -127,9 +104,7 @@ namespace Faithlife.Utility
 		/// <param name="stringBuilder">The <see cref="StringBuilder"/> instance to which the string representation will be appended.</param>
 		/// <param name="value">The value to append.</param>
 		public static StringBuilder AppendInvariant(this StringBuilder stringBuilder, uint value)
-		{
-			return stringBuilder.Append(value.ToString(CultureInfo.InvariantCulture));
-		}
+			=> stringBuilder.Append(value.ToString(CultureInfo.InvariantCulture));
 
 		/// <summary>
 		/// Appends the invariant representation of a specified 64-bit unsigned integer to the end of <paramref name="stringBuilder"/>.
@@ -137,8 +112,6 @@ namespace Faithlife.Utility
 		/// <param name="stringBuilder">The <see cref="StringBuilder"/> instance to which the string representation will be appended.</param>
 		/// <param name="value">The value to append.</param>
 		public static StringBuilder AppendInvariant(this StringBuilder stringBuilder, ulong value)
-		{
-			return stringBuilder.Append(value.ToString(CultureInfo.InvariantCulture));
-		}
+			=> stringBuilder.Append(value.ToString(CultureInfo.InvariantCulture));
 	}
 }
