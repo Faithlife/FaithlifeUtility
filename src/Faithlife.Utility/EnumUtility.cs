a0adc1a1df2f8a0046d9c3eb4ff74622f8b617e0
Ed Ball <ed.ball@faithlife.com>
2021-02-18 13:42:55 -0800

    Restrict EnumUtility to Enum.

--- src/Faithlife.Utility/EnumUtility.cs
+++ src/Faithlife.Utility/EnumUtility.cs
@@ -13,7 +13,7 @@ namespace Faithlife.Utility
 		/// <typeparam name="T">The enumerated type.</typeparam>
 		/// <returns>The values defined by the enumerated type, sorted by value.</returns>
 		public static T[] GetValues<T>()
-			where T : struct => (T[]) Enum.GetValues(typeof(T));
+			where T : struct, Enum => (T[]) Enum.GetValues(typeof(T));
 
 		/// <summary>
 		/// Determines whether the specified value is defined.
@@ -21,7 +21,8 @@ namespace Faithlife.Utility
 		/// <typeparam name="T">The enumerated type.</typeparam>
 		/// <param name="value">The value.</param>
 		/// <returns><c>true</c> if the specified value is defined; otherwise, <c>false</c>.</returns>
-		public static bool IsDefined<T>(T value) => Enum.IsDefined(typeof(T), value!);
+		public static bool IsDefined<T>(T value)
+			where T : struct, Enum => Enum.IsDefined(typeof(T), value!);
 
 		/// <summary>
 		/// Parses the specified string.
@@ -31,7 +32,7 @@ namespace Faithlife.Utility
 		/// <returns>A strongly typed enumerated value.</returns>
 		/// <remarks>This method matches case.</remarks>
 		public static T Parse<T>(string value)
-			where T : struct => Parse<T>(value, CaseSensitivity.MatchCase);
+			where T : struct, Enum => Parse<T>(value, CaseSensitivity.MatchCase);
 
 		/// <summary>
 		/// Parses the specified string.
@@ -41,7 +42,7 @@ namespace Faithlife.Utility
 		/// <param name="caseSensitivity">The case sensitivity.</param>
 		/// <returns>A strongly typed enumerated value.</returns>
 		public static T Parse<T>(string value, CaseSensitivity caseSensitivity)
-			where T : struct => (T) Enum.Parse(typeof(T), value, caseSensitivity == CaseSensitivity.IgnoreCase);
+			where T : struct, Enum => (T) Enum.Parse(typeof(T), value, caseSensitivity == CaseSensitivity.IgnoreCase);
 
 		/// <summary>
 		/// Attempts to parse the specified string.
@@ -51,7 +52,7 @@ namespace Faithlife.Utility
 		/// <returns>A strongly typed enumerated value; or null if the string could not be successfully parsed.</returns>
 		/// <remarks>This method matches case.</remarks>
 		public static T? TryParse<T>(string? value)
-			where T : struct => TryParse<T>(value, CaseSensitivity.MatchCase);
+			where T : struct, Enum => TryParse<T>(value, CaseSensitivity.MatchCase);
 
 		/// <summary>
 		/// Attempts to parse the specified string.
@@ -61,7 +62,7 @@ namespace Faithlife.Utility
 		/// <param name="caseSensitivity">The case sensitivity.</param>
 		/// <returns>A strongly typed enumerated value; null if the string could not be successfully parsed.</returns>
 		public static T? TryParse<T>(string? value, CaseSensitivity caseSensitivity)
-			where T : struct => Enum.TryParse<T>(value, caseSensitivity == CaseSensitivity.IgnoreCase, out var result) ? result : default(T?);
+			where T : struct, Enum => Enum.TryParse<T>(value, caseSensitivity == CaseSensitivity.IgnoreCase, out var result) ? result : default(T?);
 
 		/// <summary>
 		/// Attempts to parse the specified string.
@@ -72,7 +73,7 @@ namespace Faithlife.Utility
 		/// <returns>True if the string was successfully parsed.</returns>
 		/// <remarks>This method matches case.</remarks>
 		public static bool TryParse<T>(string? value, out T result)
-			where T : struct => TryParse(value, CaseSensitivity.MatchCase, out result);
+			where T : struct, Enum => TryParse(value, CaseSensitivity.MatchCase, out result);
 
 		/// <summary>
 		/// Attempts to parse the specified string.
@@ -84,6 +85,6 @@ namespace Faithlife.Utility
 		/// <returns>True if the string was successfully parsed.</returns>
 		/// <remarks>This method ignores case.</remarks>
 		public static bool TryParse<T>(string? value, CaseSensitivity caseSensitivity, out T result)
-			where T : struct => Enum.TryParse(value, caseSensitivity == CaseSensitivity.IgnoreCase, out result);
+			where T : struct, Enum => Enum.TryParse(value, caseSensitivity == CaseSensitivity.IgnoreCase, out result);
 	}
 }
