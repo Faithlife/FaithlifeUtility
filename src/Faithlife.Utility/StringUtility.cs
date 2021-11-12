01b6b69bdb63d2b1a870fcb8ba1a7d50d6600bad
Ty Mick <Ty.Mick@faithlife.com>
2021-11-12 15:29:38 -0800

    Add `ContainsAll` with `StringComparison` param
    Didn't think it would be desired enough in .NET Standard 2.0 to bother
    adding our own implementation of
    `String.Contains(String, StringComparison)`.

--- src/Faithlife.Utility/StringUtility.cs
+++ src/Faithlife.Utility/StringUtility.cs
@@ -910,6 +910,11 @@ namespace Faithlife.Utility
 		public static bool ContainsAllOrdinal(this string source, params string[] values) =>
 			source.ContainsAllOrdinal(values.AsEnumerable());
 
+#if !NETSTANDARD2_0
+		public static bool ContainsAll(this string source, IEnumerable<string> values, StringComparison comparisonType) =>
+			values.All(value => source.Contains(value, comparisonType));
+#endif
+
 		private sealed class CompressingTextWriter : TextWriter
 		{
 			public CompressingTextWriter(Stream stream, Ownership ownership)
